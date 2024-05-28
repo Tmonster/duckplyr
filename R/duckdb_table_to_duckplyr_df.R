87816ab5e9a2aaac46764e3221710dd887644009
Tmonster <tom@ebergen.com>
2024-05-28 16:26:49 +0200

    add a test

---
+++ R/duckdb_table_to_duckplyr_df.R
@@ -0,0 +1,20 @@
+#' create a duckplyr df from a table in a duckdb connection
+#'
+#' The con object **MUST** be the default con for duckplyr
+#' The DUCKPLYR_META_SKIP setting will be off for this to work
+#'
+#' @param con a connection to a duckdb database
+#' @param table_name the name of the table in the duckdb database that will be referenced as a duckplyr df
+#'
+#' @return An object of class `"duckplyr_df"`
+#'
+table_to_duckplyr_df <- function(con, table_name) {
+	# TODO: better comparison to make sure the connections are **Actually** the same.
+	if (get_default_duckdb_connection()@driver@dbdir != con@driver@dbdir) {
+		error("the passed connection is not the default duckplyr connection")
+	}
+    rel <- duckdb:::rel_from_table(con, table_name) 
+    altrep_rel <- duckdb:::rel_to_altrep(rel)
+    altrep_df <- as_duckplyr_df(altrep_rel)
+    altrep_df
+}
---
+++ tests/testthat/test-table-to-duckplyr-df.R
@@ -0,0 +1,42 @@
+test_that("duckdb_rel_from_df() uses materialized results", {
+  skip_if(identical(Sys.getenv("R_COVR"), "true"))
+
+  withr::local_envvar(DUCKPLYR_FORCE = TRUE)
+  withr::local_options(duckdb.materialize_message = TRUE)
+
+  df <-
+    data.frame(a = 1) %>%
+    duckplyr_filter(a == 1)
+
+  expect_snapshot(transform = function(x) gsub("0x[0-9a-f]+", "0xdeadbeef", x), {
+    duckdb_rel_from_df(df)
+    nrow(df)
+    duckdb_rel_from_df(df)
+  })
+})
+
+test_that("duckdb table to duckplyr df works", {
+ 
+  prev_meta_skip_val <- Sys.getenv("DUCKPLYR_META_SKIP")
+  Sys.setenv("DUCKPLYR_META_SKIP"=TRUE)
+
+  drv <- duckdb::duckdb("")
+  con <- DBI::dbConnect(drv)
+  
+  DBI::dbExecute(con, "create table foo as select range a from range(100)")
+  duckplyr:::set_default_duckdb_connection(con)
+  foo_df <- table_to_duckplyr_df(con, "foo")
+
+  expect_false(duckdb$df_is_materialized(foo_df))
+  filtered <- foo_df %>% filter(a > 90)
+
+  expect_false(duckdb$df_is_materialized(filtered))
+  answer <- as_duckplyr_df(data.frame(a=c(91, 92, 93, 94, 95, 96, 97, 98, 99)))
+  dim(filtered)
+  expect_equal(
+    answer,
+    filtered
+  )
+
+  Sys.setenv("DUCKPLYR_META_SKIP"=prev_meta_skip_val)
+})
